ae39ea51cc3f4507110c909ef3a3567269863b9f
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-07-15 02:03:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+const num3 = 17;
+const num4 = 13;
+const mod = (num3 % num4)
+const numberSet = [10, 5, 20, 15];
+const max = Math.max(...numberSet);
